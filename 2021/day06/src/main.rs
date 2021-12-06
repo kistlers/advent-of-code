ff6870cdead9288a9f8aed0410a0b6f028203079
Severin Kistler <kistler@ubique.ch>
2021-12-06 09:20:31 +0100

    2021/day06

--- 2021/day06/src/main.rs
+++ 2021/day06/src/main.rs
@@ -1,35 +1,27 @@
+use std::collections::HashMap;
+
 // Code for the problem https://adventofcode.com/2021/day/6
 const YEAR: u32 = 2021;
 const DAY: u32 = 6;
 
 fn main() {
     let input_lines: Vec<String> = elf::get_input(YEAR, DAY, "53616c7465645f5fc3caba04b37cfdc549d664f17097aa5457af16a74fbc99744bebc15d2bc22c9784c1224456ac0746");
-    let init_state = input_lines.get(0).unwrap().split(",").map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>();
-    let mut state = init_state.clone();
+    let init_state = input_lines.get(0).unwrap().split(",").map(|s| s.parse::<i64>().unwrap()).collect::<Vec<_>>();
 
-    for _i in 0..80 {
-        // println!("i: {}", i);
-        // for s in &state {
-        //     print!("{},", s);
-        // }
-        let mut new_state = vec![];
-
-        for s in state.iter_mut() {
-            // print!("*s: {},", *s);
-            if *s == 0 {
-                *s = 6;
-                new_state.push(8);
-            } else {
-                *s -= 1;
-            }
-            // print!("*s: {},", *s);
-        }
-        // println!();
-        // for s in &new_state {
-        //     print!("{},", s);
-        // }
-        state.extend(new_state);
-        // println!();
+    let mut to_spawn = (0..=8).map(|d| (d, 0)).collect::<HashMap<i64, i64>>();
+    for s in &init_state {
+        to_spawn.insert(*s, to_spawn[s] + 1);
     }
-    println!("Part1: {}", state.len());
+
+    for i in 1..=256 {
+        let new_counts: HashMap<i64, i64> = (0..=8).map(|d| match d {
+            8 => (d, to_spawn[&0]),
+            6 => (d, to_spawn[&7] + to_spawn[&0]),
+            _ => (d, to_spawn[&(d + 1)])
+        }).collect();
+        to_spawn = new_counts;
+        if [80, 256].contains(&i) {
+            println!("after day {}, {:?}", i, to_spawn.values().sum::<i64>());
+        }
+    }
 }
