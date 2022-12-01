928ca6caa3a675b8e9026b8b1206bd1ae5cd3757
Severin Kistler <kistlerseverin@gmail.com>
2022-12-01 09:36:26 +0100

    2022/day01

---
+++ 2022/day01/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::BinaryHeap;
+
+// Code for the problem https://adventofcode.com/2022/day/01
+const YEAR: u32 = 2022;
+const DAY: u32 = 01;
+
+fn main() {
+    let input_lines: Vec<String> = elf::get_input(YEAR, DAY, "53616c7465645f5f235e9a21eb1ad1558a877268d5a6533d8a3c56c539f0886b51e27e5ccf230b8815cb5ca121c1f3173a16dd022c3803eefcd5e6fb5f077dae");
+    let top_calories = &input_lines
+        .join(" ")
+        .split("  ")
+        .map(|foods| {
+            foods
+                .split(" ")
+                .map(|cal| cal.parse::<i32>().unwrap())
+                .sum::<i32>()
+        })
+        .collect::<BinaryHeap<_>>()
+        .into_vec()[0..3];
+    println!("part1: {}", top_calories[0]);
+    println!("part2: {}", top_calories.iter().sum::<i32>());
+}
