cf4fb1619de2a038a5fdaa8d6711be67585f6a8b
Severin Kistler <kistlerseverin@gmail.com>
2022-12-03 14:48:35 +0100

    2022/day03

---
+++ 2022/day03/src/main.rs
@@ -0,0 +1,32 @@
+use std::collections::HashSet;
+
+
+// Code for the problem https://adventofcode.com/2022/day/03
+const YEAR: u32 = 2022;
+const DAY: u32 = 03;
+
+fn main() {
+    let input_lines: Vec<String> = elf::get_input(YEAR, DAY, "53616c7465645f5f235e9a21eb1ad1558a877268d5a6533d8a3c56c539f0886b51e27e5ccf230b8815cb5ca121c1f3173a16dd022c3803eefcd5e6fb5f077dae");
+    let part1 = input_lines.iter()
+        .map(|input_line| {
+            let (left, right) = input_line.split_at(input_line.len() / 2);
+            let left_chars = left.chars().collect::<HashSet<char>>();
+            let right_chars = right.chars().collect::<HashSet<char>>();
+            let intersection = *(&left_chars & &right_chars).into_iter().collect::<Vec<char>>().first().unwrap();
+            priority(intersection)
+        }).sum::<u32>();
+    println!("part1: {}", part1);
+
+    let part2 = input_lines.chunks(3)
+        .map(|chunk| {
+            let packs = chunk.iter()
+                .map(|c| c.chars().collect::<HashSet<char>>()).collect::<Vec<HashSet<char>>>();
+            let intersection = *(&(&packs[0] & &packs[1]) & &packs[2]).into_iter().collect::<Vec<char>>().first().unwrap();
+            priority(intersection)
+        }).sum::<u32>();
+    println!("part2: {}", part2);
+}
+
+fn priority(c: char) -> u32 {
+    if c.is_uppercase() { c as u32 - 'A' as u32 + 27 } else { c as u32 - 'a' as u32 + 1 }
+}
