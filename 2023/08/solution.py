3931d4949a57aff6ff6cf366cbe10a445e63cbef
Severin Kistler <kistlerseverin@gmail.com>
2023-12-09 17:44:35 +0100

    2023 08 part 2

--- 2023/08/solution.py
+++ 2023/08/solution.py
@@ -1,12 +1,28 @@
 import itertools
+import math
 from typing import Iterable
 
 from utils import run
 import re
 
 
+def find_steps(instructions: list[str], neighbours: dict[str, (str, str)], start_node: str,
+               end_nodes: list[str]) -> int:
+    current = start_node
+    for step, instruction in enumerate(itertools.cycle(instructions)):
+        if current in end_nodes:
+            return step
+        elif instruction == 'L':
+            current = neighbours[current][0]
+        elif instruction == 'R':
+            current = neighbours[current][1]
+        pass
+
+
 def solve(input_data: str) -> Iterable[int]:
     lines = input_data.splitlines()
+
+    instructions = list(lines[0])
 
     neighbours = dict()
     for line in lines[2:]:
@@ -14,23 +30,12 @@ def solve(input_data: str) -> Iterable[int]:
         print(f"{node} -> {left}, {right}")
         neighbours[node] = (left, right)
 
-    print(neighbours)
+    yield find_steps(instructions, neighbours, 'AAA', ['ZZZ'])
 
-    current = 'AAA'
-    target = 'ZZZ'
-    steps = 0
-    for i, instruction in enumerate(itertools.cycle(list(lines[0]))):
-        if current == target:
-            steps = i
-            break
-        elif instruction == 'L':
-            current = neighbours[current][0]
-        elif instruction == 'R':
-            current = neighbours[current][1]
-
-        pass
-
-    yield steps
+    a_nodes = list(filter(lambda n: n.endswith('A'), neighbours.keys()))
+    z_nodes = list(filter(lambda n: n.endswith('Z'), neighbours.keys()))
+    steps = [find_steps(instructions, neighbours, a_node, z_nodes) for a_node in a_nodes]
+    yield math.lcm(*steps)
 
 
 run(solve)
