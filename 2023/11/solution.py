8287bd1f2b3188022c278b1ade25ce7b0c3fbd96
Severin Kistler <kistlerseverin@gmail.com>
2023-12-12 08:45:26 +0100

    2023 12

--- 2023/11/solution.py
+++ 2023/11/solution.py
@@ -4,43 +4,30 @@ from typing import Iterable
 from utils import run
 
 
-def transpose(list_of_list: list[list[str]]):
-    return list(map(list, itertools.zip_longest(*list_of_list, fillvalue=None)))
-
-
 def solve(input_data: str) -> Iterable[int]:
     initial_grid = [list(line) for line in input_data.splitlines()]
     empty_rows = set([i for i, row in enumerate(initial_grid) if all(v == '.' for v in row)])
-    print(empty_rows)
-    transposed_initial = transpose(initial_grid)
+    transposed_initial = list(map(list, itertools.zip_longest(*initial_grid, fillvalue=None)))
     empty_columns = set([j for j, col in enumerate(transposed_initial) if all(v == '.' for v in col)])
-    print(empty_columns)
 
-    for row in initial_grid:
-        print("".join(row))
-
-    print()
-
-    grid = initial_grid
-    for i in sorted(empty_rows, reverse=True):
-        grid.insert(i, ['.'] * len(grid[0]))
-    grid = transpose(grid)
-    for j in sorted(empty_columns, reverse=True):
-        grid.insert(j, ['.'] * len(grid[0]))
-    grid = transpose(grid)
-
-    for row in grid:
-        print("".join(row))
-
-    galaxies = [(i, j) for i, row in enumerate(grid) for j, v in enumerate(row) if v == '#']
-    path_sum = 0
+    galaxies = [(i, j) for i, row in enumerate(initial_grid) for j, v in enumerate(row) if v == '#']
+    initial_path_sum = 0
+    added_rows_cols = 0
     for (g1i, g1j), (g2i, g2j) in itertools.combinations(galaxies, 2):
-        path = abs(g2i - g1i) + abs(g2j - g1j)
-        path_sum += path
-        print(
-            f"({g1i}, {g1j}) -> ({g2i}, {g2j}) =  {path}")
-    print(path_sum)
-    yield path_sum
+        i_from = min(g1i, g2i)
+        i_to = max(g1i, g2i)
+        j_from = min(g1j, g2j)
+        j_to = max(g1j, g2j)
+        initial_path = (i_to - i_from) + (j_to - j_from)
+        added_rows = len(set(range(i_from, i_to + 1)).intersection(empty_rows))
+        added_columns = len(set(range(j_from, j_to + 1)).intersection(empty_columns))
+        initial_path_sum += initial_path
+        added_rows_cols += added_rows + added_columns
+    part_1_sum = initial_path_sum + added_rows_cols
+    part_2_sum = initial_path_sum + (1000000 - 1) * added_rows_cols
+
+    yield part_1_sum
+    yield part_2_sum
 
 
 run(solve)
