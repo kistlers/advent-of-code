d58f5cdc8182a1c8ecd603d775db4df98a448c3b
Severin Kistler <kistlerseverin@gmail.com>
2024-12-01 10:20:55 +0100

    better test setup with expected answers for part1/part2

--- elixir/test/advent_of_code/solution/year_2019/day_25_test.exs
+++ elixir/test/advent_of_code/solution/year_2019/day_25_test.exs
@@ -5,22 +5,31 @@ defmodule AdventOfCode.Solution.Year2019.Day25Test do
 
   setup do
     [
-      input: """
-      """
+      test_cases: [
+        %{
+          input: """
+          0
+          """,
+          expected_part1: 1,
+          expected_part2: 2
+        }
+      ]
     ]
   end
 
   @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
-
-    assert result
+  test "part1", %{test_cases: test_cases} do
+    Enum.each(test_cases, fn %{input: input, expected_part1: expected} ->
+      result = part1(input)
+      assert result == expected
+    end)
   end
 
   @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
-
-    assert result
+  test "part2", %{test_cases: test_cases} do
+    Enum.each(test_cases, fn %{input: input, expected_part2: expected} ->
+      result = part2(input)
+      assert result == expected
+    end)
   end
 end
