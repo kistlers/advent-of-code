e2dc0ca2d2f306dfb2d981bae9b5485592646320
Severin Kistler <kistlerseverin@gmail.com>
2024-12-01 10:20:38 +0100

    2024/01

--- elixir/lib/advent_of_code/solution/year_2024/day_01.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_01.ex
@@ -1,7 +1,40 @@
 defmodule AdventOfCode.Solution.Year2024.Day01 do
-  def part1(_input) do
+  def part1(input) do
+    columns = parse_to_sorted_columns(input)
+
+    columns
+    |> Stream.zip()
+    |> Enum.map(fn {a, b} -> abs(b - a) end)
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    columns = parse_to_sorted_columns(input)
+
+    left = Enum.at(columns, 0)
+    right = Enum.at(columns, 1)
+
+    left
+    |> Stream.map(fn left_item ->
+      left_item * Enum.count(right, fn right_item -> right_item == left_item end)
+    end)
+    |> Enum.sum()
+  end
+
+  def parse_to_sorted_columns(input) do
+    input
+    |> String.trim()
+    |> String.split("\n", trim: true)
+    |> Stream.map(fn
+      line ->
+        String.split(line, ~r/\s+/, trim: true)
+        |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.to_list()
+    # transpose rows to columns
+    |> Stream.zip()
+    |> Stream.map(&Tuple.to_list/1)
+    |> Stream.map(&Enum.sort/1)
+    |> Enum.to_list()
   end
 end
--- elixir/test/advent_of_code/solution/year_2024/day_01_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_01_test.exs
@@ -5,22 +5,36 @@ defmodule AdventOfCode.Solution.Year2024.Day01Test do
 
   setup do
     [
-      input: """
-      """
+      test_cases: [
+        %{
+          input: """
+          3   4
+          4   3
+          2   5
+          1   3
+          3   9
+          3   3
+          """,
+          expected_part1: 11,
+          expected_part2: 31
+        }
+      ]
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
-
-    assert result
+  # @tag :skip
+  test "part1", %{test_cases: test_cases} do
+    Enum.each(test_cases, fn %{input: input, expected_part1: expected} ->
+      result = part1(input)
+      assert result == expected
+    end)
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
-
-    assert result
+  # @tag :skip
+  test "part2", %{test_cases: test_cases} do
+    Enum.each(test_cases, fn %{input: input, expected_part2: expected} ->
+      result = part2(input)
+      assert result == expected
+    end)
   end
 end
