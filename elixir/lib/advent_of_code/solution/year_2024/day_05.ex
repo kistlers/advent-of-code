8c38c5b4895791eedfb1ea2c8d0ae77d19c7fd39
Severin Kistler <kistlerseverin@gmail.com>
2024-12-05 20:39:34 +0100

    2024/05

--- elixir/lib/advent_of_code/solution/year_2024/day_05.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_05.ex
@@ -1,7 +1,96 @@
 defmodule AdventOfCode.Solution.Year2024.Day05 do
-  def part1(_input) do
+  def part1(input) do
+    {rules, updates} = parse_input(input)
+
+    find_valid_or_invalid(rules, updates, true)
+    |> Enum.map(fn update -> Enum.at(update, div(length(update), 2)) end)
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    {rules, updates} = parse_input(input)
+
+    find_valid_or_invalid(rules, updates, false)
+    |> Enum.map(fn update ->
+      graph = build_graph_subset(rules, update)
+
+      Enum.sort(update, &edge_exists?(graph, &1, &2))
+    end)
+    |> Enum.map(fn update -> Enum.at(update, div(length(update), 2)) end)
+    |> Enum.sum()
   end
+
+  defp parse_input(input) do
+    [split_rules, split_updates] =
+      input
+      |> String.trim()
+      |> String.split("\n\n", trim: true)
+
+    rules =
+      split_rules
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn rule ->
+        rule
+        |> String.split("|", trim: true)
+        |> Enum.map(&String.to_integer/1)
+        |> List.to_tuple()
+      end)
+
+    updates =
+      split_updates
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn update ->
+        update
+        |> String.split(",", trim: true)
+        |> Enum.map(&String.to_integer/1)
+      end)
+
+    {rules, updates}
+  end
+
+  defp find_valid_or_invalid(rules, updates, find_valid) do
+    updates
+    |> Enum.filter(fn update ->
+      graph = build_graph_subset(rules, update)
+      update_is_valid(graph, update) == find_valid
+    end)
+  end
+
+  defp build_graph_subset(rules, update) do
+    rules
+    |> Enum.filter(fn {from, to} -> Enum.member?(update, from) && Enum.member?(update, to) end)
+    |> to_adjacency_list()
+  end
+
+  defp update_is_valid(graph, update) do
+    Enum.chunk_every(update, 2, 1, :discard)
+    |> Enum.all?(fn [from, to] ->
+      edge_exists?(graph, from, to) and not edge_exists?(graph, to, from)
+    end)
+  end
+
+  defp to_adjacency_list(edges) do
+    Enum.reduce(edges, %{}, fn {from, to}, acc ->
+      Map.update(acc, from, MapSet.new([to]), &MapSet.put(&1, to))
+    end)
+  end
+
+  defp edge_exists?(graph, from, to) do
+    bfs(graph, [from], MapSet.new(), to)
+  end
+
+  defp bfs(_graph, [], _visited, _to), do: false
+
+  defp bfs(graph, [current | queue], visited, to) do
+    cond do
+      current == to -> true
+      true ->
+        neighbors = Map.get(graph, current, MapSet.new())
+        unvisited_neighbors = MapSet.difference(neighbors, visited)
+
+        # Use the list of unvisited neighbors as the new queue, avoiding `++` for efficiency
+        bfs(graph, MapSet.to_list(unvisited_neighbors) ++ queue, MapSet.put(visited, current), to)
+    end
+  end
+
 end
--- elixir/test/advent_of_code/solution/year_2024/day_05_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_05_test.exs
@@ -8,16 +8,43 @@ defmodule AdventOfCode.Solution.Year2024.Day05Test do
       test_cases: [
         %{
           input: """
-          0
+          47|53
+          97|13
+          97|61
+          97|47
+          75|29
+          61|13
+          75|53
+          29|13
+          97|29
+          53|29
+          61|53
+          97|53
+          61|29
+          47|13
+          75|47
+          97|75
+          47|61
+          75|61
+          47|29
+          75|13
+          53|13
+
+          75,47,61,53,29
+          97,61,53,29,13
+          75,29,13
+          75,97,47,61,53
+          61,13,29
+          97,13,75,29,47
           """,
-          expected_part1: 1,
-          expected_part2: 2
+          expected_part1: 143,
+          expected_part2: 123
         }
       ]
     ]
   end
 
-  @tag :skip
+#  @tag :skip
   test "part1", %{test_cases: test_cases} do
     Enum.each(test_cases, fn %{input: input, expected_part1: expected} ->
       result = part1(input)
@@ -25,7 +52,7 @@ defmodule AdventOfCode.Solution.Year2024.Day05Test do
     end)
   end
 
-  @tag :skip
+#  @tag :skip
   test "part2", %{test_cases: test_cases} do
     Enum.each(test_cases, fn %{input: input, expected_part2: expected} ->
       result = part2(input)
