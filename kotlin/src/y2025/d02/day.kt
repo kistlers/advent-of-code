dc2fc4ed1db7f0f319ff61fa5cb122d79eccbebd
Severin Kistler <kistlerseverin@gmail.com>
2025-12-02 22:34:02 +0100

    2025/02

--- kotlin/src/y2025/d02/day.kt
+++ kotlin/src/y2025/d02/day.kt
@@ -9,18 +9,40 @@ fun main() {
     val year = "2025"
     val day = "02"
 
-    fun part1(input: List<String>): Int = input.size
+    fun allIds(input: List<String>): List<Long> =
+        input[0].split(",").flatMap {
+            val split = it.split("-")
+            (split[0].toLong()..split[1].toLong()).toList()
+        }
 
-    fun part2(input: List<String>): Int = input.size
+    fun isRepeatingNTimes(s: String, n: Int): Boolean =
+        s.length % n == 0 && s == s.take(s.length / n).repeat(n)
+
+    fun part1(input: List<String>): Long =
+        allIds(input)
+            .filter {
+                val s = it.toString()
+                // if the length is even, and first half == second half
+                isRepeatingNTimes(s, 2)
+            }
+            .sum()
+
+    fun part2(input: List<String>): Long =
+        allIds(input)
+            .filter {
+                val s = it.toString()
+                (2..s.length).any { n -> isRepeatingNTimes(s, n) }
+            }
+            .sum()
 
     val testInput = readTest(year, day)
     val input = readInput(year, day)
 
     val part1Test = part1(testInput)
-    checkTest(3, part1Test)
+    checkTest(1227775554, part1Test)
     part1(input).println()
 
     val part2Test = part2(testInput)
-    checkTest(6, part2Test)
+    checkTest(4174379265, part2Test)
     part2(input).println()
 }
