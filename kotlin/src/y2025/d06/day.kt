dcc56cf52eeac2cccc6d96b6fa0f693c3e9d12cd
Severin Kistler <kistlerseverin@gmail.com>
2025-12-02 21:59:40 +0100

    add empty days 2025 02-12

---
+++ kotlin/src/y2025/d06/day.kt
@@ -0,0 +1,26 @@
+package y2025.d06
+
+import checkTest
+import println
+import readInput
+import readTest
+
+fun main() {
+    val year = "2025"
+    val day = "06"
+
+    fun part1(input: List<String>): Int = input.size
+
+    fun part2(input: List<String>): Int = input.size
+
+    val testInput = readTest(year, day)
+    val input = readInput(year, day)
+
+    val part1Test = part1(testInput)
+    checkTest(3, part1Test)
+    part1(input).println()
+
+    val part2Test = part2(testInput)
+    checkTest(6, part2Test)
+    part2(input).println()
+}
---
+++ kotlin/src/y2025/d12/day.kt
@@ -0,0 +1,26 @@
+package y2025.d12
+
+import checkTest
+import println
+import readInput
+import readTest
+
+fun main() {
+    val year = "2025"
+    val day = "12"
+
+    fun part1(input: List<String>): Int = input.size
+
+    fun part2(input: List<String>): Int = input.size
+
+    val testInput = readTest(year, day)
+    val input = readInput(year, day)
+
+    val part1Test = part1(testInput)
+    checkTest(3, part1Test)
+    part1(input).println()
+
+    val part2Test = part2(testInput)
+    checkTest(6, part2Test)
+    part2(input).println()
+}
