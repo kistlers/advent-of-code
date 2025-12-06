347b40825ef8584994756039d689a65c7e8c93fe
Severin Kistler <kistlerseverin@gmail.com>
2025-12-06 11:43:13 +0100

    2025/05

--- kotlin/src/y2025/d05/day.kt
+++ kotlin/src/y2025/d05/day.kt
@@ -9,18 +9,55 @@ fun main() {
     val year = "2025"
     val day = "05"
 
-    fun part1(input: List<String>): Long = input.size.toLong()
+    fun parseInput(input: List<String>): Pair<List<ClosedRange<Long>>, List<Long>> {
+        val emptyLineIndex = input.indexOf("")
+        val ranges =
+            input.take(emptyLineIndex).map {
+                val from = it.substringBefore("-").toLong()
+                val to = it.substringAfter("-").toLong()
+                from..to
+            }
+        val ids = input.drop(emptyLineIndex + 1).map { it.toLong() }
+        return ranges to ids
+    }
 
-    fun part2(input: List<String>): Long = input.size.toLong()
+    fun ClosedRange<Long>.countLong(): Long = endInclusive - start + 1
+
+    fun part1(input: List<String>): Long {
+        val (ranges, ids) = parseInput(input)
+
+        return ids.count { id -> ranges.any { range -> id in range } }.toLong()
+    }
+
+    fun part2(input: List<String>): Long {
+        val (ranges, _) = parseInput(input)
+
+        return ranges
+            .sortedWith(compareBy({ it.start }, { it.endInclusive }))
+            .fold(0L to -1L) { (idCount, end), range ->
+                // the current range is fully after the current end -> count all
+                if (end < range.start) {
+                    return@fold idCount + range.countLong() to range.endInclusive
+                }
+
+                // the current range is fully covered -> count no elements
+                if (end >= range.endInclusive) {
+                    return@fold idCount to range.endInclusive
+                }
+
+                // partially covered, count elements in range [end+1, range.endInclusive]
+                return@fold idCount + (end + 1..range.endInclusive).countLong() to
+                    range.endInclusive
+            }
+            .first
+    }
 
     val testInput = readTest(year, day)
     val input = readInput(year, day)
 
-    val part1Test = part1(testInput)
-    checkTest(3, part1Test)
+    checkTest(3, part1(testInput))
     part1(input).println()
 
-    val part2Test = part2(testInput)
-    checkTest(6, part2Test)
+    checkTest(14, part2(testInput))
     part2(input).println()
 }
