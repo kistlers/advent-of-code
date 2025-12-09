216a6384b4ea4c006a313c5d0d855ee067724d54
Severin Kistler <kistlerseverin@gmail.com>
2025-12-09 20:11:27 +0100

    2025/09

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -2,6 +2,14 @@ plugins {
     kotlin("jvm") version "2.2.21"
 }
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.locationtech.jts:jts-core:latest.release")
+}
+
 sourceSets {
     main {
         kotlin.srcDir("src")
--- kotlin/src/y2025/d09/day.kt
+++ kotlin/src/y2025/d09/day.kt
@@ -1,6 +1,8 @@
 package y2025.d09
 
 import checkTest
+import org.locationtech.jts.geom.Coordinate
+import org.locationtech.jts.geom.GeometryFactory
 import println
 import readInput
 import readTest
@@ -9,16 +11,59 @@ fun main() {
     val year = "2025"
     val day = "09"
 
-    fun part1(input: List<String>): Long = input.size.toLong()
+    val geometryFactory = GeometryFactory()
 
-    fun part2(input: List<String>): Long = input.size.toLong()
+    fun List<String>.parsePoints(): List<Pair<Double, Double>> =
+        this.map { it.split(",").let { (x, y) -> x.toDouble() to y.toDouble() } }
+
+    fun solve(input: List<String>): Pair<Long, Long> {
+        val points = input.parsePoints()
+
+        val ring = points.map { Coordinate(it.first, it.second) }.toMutableList()
+        ring.add(ring.first())
+        val polygon = geometryFactory.createPolygon(ring.toTypedArray())
+
+        val rectangles =
+            points.flatMapIndexed { i, p1 ->
+                points.drop(i + 1).map { p2 ->
+                    val (x1, y1) = p1
+                    val (x2, y2) = p2
+                    val rectangle =
+                        geometryFactory.createPolygon(
+                            geometryFactory.createLinearRing(
+                                arrayOf(
+                                    Coordinate(x1, y1),
+                                    Coordinate(x2, y1),
+                                    Coordinate(x2, y2),
+                                    Coordinate(x1, y2),
+                                    Coordinate(x1, y1),
+                                )
+                            )
+                        )
+
+                    // our rectangle is only aligned in the center between the grid points. We need
+                    // to add a 0.5 wide boundary to the outside, which is
+                    // 2*height*0.5 + 2*width*0.5 + 4*1/4 grid cells == boundary/2+1
+                    val area = rectangle.area + rectangle.boundary.length / 2 + 1
+                    val isInPolygon = polygon.contains(rectangle)
+                    area to isInPolygon
+                }
+            }
+
+        val part1 = rectangles.maxOf { it.first }
+        val part2 = rectangles.filter { it.second }.maxOf { it.first }
+        return part1.toLong() to part2.toLong()
+    }
 
     val testInput = readTest(year, day)
     val input = readInput(year, day)
 
-    checkTest(3, part1(testInput))
-    part1(input).println()
+    val (test1, test2) = solve(testInput)
+    val (part1, part2) = solve(input)
 
-    checkTest(6, part2(testInput))
-    part2(input).println()
+    checkTest(50, test1)
+    part1.println()
+
+    checkTest(24, test2)
+    part2.println()
 }
